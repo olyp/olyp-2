d40b89dedde924f0efa5efc3a9b6301acd25b74f
Jonas Barsten <jonasbarsten@gmail.com>
2017-04-06 17:19:12 +0200

    Changed preloader design

--- imports/ui/shared/preloader/Preloader.js
+++ imports/ui/shared/preloader/Preloader.js
@@ -1,14 +1,38 @@
 import React, {Component} from 'react';
+import Vivus from 'vivus';
 
 class Preloader extends Component {
+
 	render () {
+
+		// Square preloader
+
 		return (
-			<div id="loader-wrapper">
-				<div id="loader"></div>
-				<div className="loader-section section-left"></div>
-            	<div className="loader-section section-right"></div>
+			<div id="preloader-square">
+				<div className="loader">
+					<div className="square" ></div>
+					<div className="square"></div>
+					<div className="square last"></div>
+					<div className="square clear"></div>
+					<div className="square"></div>
+					<div className="square last"></div>
+					<div className="square clear"></div>
+					<div className="square "></div>
+					<div className="square last"></div>
+				</div>
 			</div>
 		);
+
+
+		// Abstract preloader
+
+		// return (
+		// 	<div id="loader-wrapper">
+		// 		<div id="loader"></div>
+		// 		<div className="loader-section section-left"></div>
+  //           	<div className="loader-section section-right"></div>
+		// 	</div>
+		// );
 	}
 }
 
