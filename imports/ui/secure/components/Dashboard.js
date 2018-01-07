3cfd6838481db4e54ab715c0d1bb8029a5b2b196
Jonas Barsten <jonasbarsten@gmail.com>
2018-01-07 16:13:22 +0100

    Adding door code to dashboard

--- imports/ui/secure/components/Dashboard.js
+++ imports/ui/secure/components/Dashboard.js
@@ -1,13 +1,46 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Glyphicon } from 'react-bootstrap';
 
-export default class Dashboard extends Component {
+import DoorCodes from '../../../api/collections/doorCodes';
+
+import Preloader from '../../shared/preloader/Preloader';
+
+class Dashboard extends Component {
 
 	render() {
+
+		const user = Meteor.users.findOne({_id: Meteor.userId()});
+
+		if (!user) {
+			return <Preloader />;
+		}
+
+		const doorCode = this.props.doorCode ?
+			<h4><Glyphicon glyph="lock" /> {this.props.doorCode.code}</h4> :
+			null;
+
 		return (
 			<div className="container">
-				<h1>Dashboard</h1>
+				<div className="row">
+					<h4 className="col-xs-12">Hei {user.profile.firstName}</h4>
+				</div>
+				<hr />
+				<div className="row">
+					<div className="col-xs-12">
+						{doorCode}
+					</div>
+				</div>
 			</div>
 			
 		);
 	}
 }
+
+export default createContainer(() => {
+	Meteor.subscribe('doorCode');
+
+	return {
+		doorCode: DoorCodes.find().fetch()[0]
+	}
+}, Dashboard);
