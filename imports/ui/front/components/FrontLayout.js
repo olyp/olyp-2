f0036a88d471ede1e8c9ac3d5250a997c4903486
Jonas Barsten <jonasbarsten@gmail.com>
2018-01-19 14:44:07 +0100

    Adding schema.org schema to main.html

--- imports/ui/front/components/FrontLayout.js
+++ imports/ui/front/components/FrontLayout.js
@@ -7,6 +7,31 @@ import NavBar from '../../shared/navigation/NavBar.js';
 import NavBarDesktop from '../../shared/navigation/NavBarDesktop.js';
 import Footer from './navigation/Footer.js';
 
+// const schemaOrg = 
+// <script type="application/ld+json"> {
+// 	"@context": "http://schema.org",
+// 	"@type": "Organization",
+// 	"url": "https://olyp.no",
+// 	"name": "Oslo Lydproduksjon (OLYP)",
+//     "logo": "https://olyp.no/images/logo/logo-square.png",
+//     "vatId": "912443760",
+//     "address": {
+//     	"@type": "PostalAddress",
+//     	"addressCountry": "NO",
+//         "postalCode": "0570",
+//         "streetAddress": "Olaf Schous Vei 6 C",
+//         "addressRegion": "Oslo",
+//         "addressLocality": "Oslo"
+//     },
+// 	"contactPoint": {
+// 		"@type": "ContactPoint",
+// 		"telephone": "+47 41547798",
+//         "email": "jonas@olyp.no",
+//         "contactType": "Customer service"
+// 	}
+// } </script>
+	
+
 class FrontLayout extends Component {
 
 	render() {
@@ -15,15 +40,16 @@ class FrontLayout extends Component {
 
 		return (
 			<div>
+				<Helmet>
+					<title>{routeName}</title>
+					<meta name="google-site-verification" content={Meteor.settings.public.googleSiteVerification} />
+				</Helmet>
 				<div className="mobile">
 					<NavBar />
 				</div>
 				<div className="desktop">
 					<NavBarDesktop />
 				</div>
-				<Helmet>
-					<title>{routeName}</title>
-				</Helmet>
 				{this.props.children}
 				<Footer />
 			</div>
